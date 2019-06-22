20a8e6c30c580b9e767f0976a4a99c0eea236055
David Sanders <dsanders11@ucsbalum.com>
2019-06-22 03:28:39 -0700

    Reproduce case for timers module not working in renderer

--- renderer.js
+++ renderer.js
@@ -1,3 +1,10 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+const { setTimeout } = require('timers');
+
+setTimeout(() => console.log('Timeout fired'), 1000);
+
+// Uncomment the line below to cause the timeout to fire
+// process.nextTick(() => {})
